098325eb45a4382c0af2f32aaf3ab61b01c09445
Bbuno <Bbuno@HACKER_PC>
2023-10-27 21:43:29 +0300

    Добавьте файлы проекта.

---
+++ Calculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5
+{
+    public class Calculator : ICalc
+    {
+        public float result = 0;
+        public event EventHandler<EventArgs> EventResult;
+
+        public float Add(int x)
+        {
+
+            result += x;
+            operation.Push(new Tuple<string, int>("+", x));
+            SendResult();
+            return result;
+        }
+
+        public float Sub(int x)
+        {
+
+            result -= x;
+            operation.Push(new Tuple<string, int>("-", x));
+
+            SendResult();
+            return result;
+        }
+
+        public float Mul(int x)
+        {
+
+            result *= x;
+            operation.Push(new Tuple<string, int>("*", x));
+
+            SendResult();
+            return result;
+        }
+
+        public float Div(int x)
+        {
+
+            result /= x;
+            operation.Push(new Tuple<string, int>("/", x));
+
+            SendResult();
+            return result;
+        }
+
+        public void CancelLast()
+        {
+            if (operation.Count == 0)
+            {
+                Console.WriteLine("Операция отмены невозможна. Нет операций для отмены.");
+                return;
+            }
+            string currentOperation = operation.Peek().Item1;
+            switch (currentOperation)
+            {
+                case "+":
+                    result -= operation.Pop().Item2;
+                    break;
+                case "-":
+                    result += operation.Pop().Item2;
+                    break;
+                case "*":
+                    result /= operation.Pop().Item2;
+                    break;
+                case "/":
+                    result *= operation.Pop().Item2;
+                    break;
+                default:
+                    Console.WriteLine("Ошибка");
+                    break;
+
+
+            }
+
+            SendResult();
+        }
+
+        private void SendResult()
+        {
+            EventResult.Invoke(this, new EventArgs());
+        }
+        public Stack<Tuple<string, int>> operation = new Stack<Tuple<string, int>>();
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var calculator = new Calculator();
+            calculator.EventResult += C_EventResult;
+            float result = 0;
+            bool a = true;
+            int num;
+            while (a)
+            {
+                Console.WriteLine("1 - операция +");
+                Console.WriteLine("2 - операция -");
+                Console.WriteLine("3 - операция /");
+                Console.WriteLine("4 - операция *");
+                Console.WriteLine("5 - отмены последней операции");
+                Console.WriteLine("6 - выход");
+                Console.Write("Ввод: ");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Введите число: ");
+                        num = int.Parse(Console.ReadLine());
+                        result = calculator.Add(num);
+                        break;
+                    case "2":
+                        Console.Write("Введите число: ");
+                        num = int.Parse(Console.ReadLine());
+                        result = calculator.Sub(num);
+                        break;
+                    case "3":
+                        Console.Write("Введите число: ");
+                        num = int.Parse(Console.ReadLine());
+                        if (num == 0)
+                        {
+                            Console.WriteLine("Ошибка! На ноль делить нельзя!");
+                            break;
+                        }
+                        result = calculator.Div(num);
+                        break;
+                    case "4":
+                        Console.Write("Введите число: ");
+                        num = int.Parse(Console.ReadLine());
+                        result = calculator.Mul(num);
+                        break;
+                    case "5":
+                        Console.WriteLine("Отмена последней операции");
+                        calculator.CancelLast();
+                        break;
+                    case "6":
+                        Console.WriteLine("Выход");
+                        a = false;
+                        break;
+                    default:
+                        Console.WriteLine("Некорректная операция. Программа завершена.");
+                        break;
+                }
+            }
+        }
+
+        private static void C_EventResult(object sender, EventArgs e)
+        {
+            Console.WriteLine(((Calculator)sender).result);
+        }
+    }
+}
